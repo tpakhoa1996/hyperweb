b3f5466f7929286b6b9f971531362c55010e5e24
Brian Tran <patra3@student.monash.edu>
2023-05-07 20:33:58 +1000

    Update css_importer.js

--- css_importer.js
+++ css_importer.js
@@ -5,6 +5,8 @@
 	let cssURL = null;
 	if (/^https?:\/\/+([^:/]+\.)?123duw\.com(\/.*)?$/.test(href)) {
 		cssURL = 'https://raw.githubusercontent.com/tpakhoa1996/hyperweb/main/css/123duw.css?hash=4';
+	} else if (/^https?:\/\/+([^:/]+\.)?bqg126\.com(\/.*)?$/.test(href)) {
+    	cssURL = 'https://raw.githubusercontent.com/tpakhoa1996/hyperweb/main/css/bqg126.css?hash=4';
 	}
 
 	if (cssURL !== null) {
