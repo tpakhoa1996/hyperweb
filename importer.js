dcd689549c023e545f97743c4699d5290ac42893
Brian Tran <patra3@student.monash.edu>
2023-05-18 14:19:44 +0000

    update

--- importer.js
+++ importer.js
@@ -21,11 +21,43 @@
 
 	let scriptURL = null;
 	if (/^https?:\/\/+([^:/]+\.)?lastestnovel\.com(\/.*)?$/.test(href)) {
-		scriptURL = 'https://raw.githubusercontent.com/tpakhoa1996/hyperweb/main/js/lastednovel.js?hash=5';
+		// scriptURL = 'https://raw.githubusercontent.com/tpakhoa1996/hyperweb/main/js/lastednovel.js?hash=5';
+		alert("testing");
+		const run = () => {
+			alert("running");
+			Array.from(document.querySelectorAll('.content-chapter > p')).forEach(p => {
+				if (p.getAttributeNames().length > 0) {
+					const beforeAttr = window.getComputedStyle(p, '::before')['content'].split(/\(|\)/)[1];
+					const afterAttr = window.getComputedStyle(p, '::after')['content'].split(/\(|\)/)[1];
+	
+					let beforeText = p.getAttribute(beforeAttr);
+					let affterText = p.getAttribute(afterAttr);
+	
+					p.setAttribute(beforeAttr, '');
+					p.setAttribute(afterAttr, '');
+	
+					if (beforeText.endsWith('null')) {
+						beforeText = beforeText.slice(0, -4);
+					}
+	
+					if (affterText.startsWith('null')) {
+						affterText = affterText.slice(4);
+					}
+	
+					p.textContent = beforeText + p.textContent + affterText;
+				}
+			});
+		}
+	
+		if (document.readyState === 'complete') {
+			run();
+		} else {
+			window.addEventListener('load', run);
+		}
 	}
 
-	if (scriptURL !== null) {
-		alert("Runing Script");
-		eval(await (await fetch(scriptURL)).text());
-	}
+	// if (scriptURL !== null) {
+	// 	alert("Runing Script");
+	// 	eval(await (await fetch(scriptURL)).text());
+	// }
 })();
